455d5c0886f0d43f83d336f31cc04b5d38d48d07
Muhammad <muhammadevvv@gmail.com>
2024-10-29 13:21:07 +0500

    uzum clone

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  
+  console.log(count)
+  return (
+    <>
+      <h1 className=''></h1>
+    </>
+  )
+}
+
+export default App
